21876aa35c8242b1f8fc5b5252a23067cd2b7614
RagingLightning <RagingLightningCode@gmail.com>
2023-06-08 18:38:37 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TS3_Airport_Editor;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+}
